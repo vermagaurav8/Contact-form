26c6e58be4055b37e79a63e10136754e6c0c3bd9
Gaurav Verma <gaurav.verma382001@gmail.com>
2023-04-21 12:57:16 +0530

    created basic html form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function ContactForm() {
+  return (
+    <form>
+        <div>
+            <label htmlFor='name'>Name:</label>
+            <input type='text' id='name' required />
+        </div>
+        <div>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' required />
+        </div>
+        <div>
+            <label htmlFor='message'>Message:</label>
+            <textarea id='message' required />
+        </div>
+        <button type='submit'></button>
+    </form>
+  )
+}
+
+export default ContactForm;
