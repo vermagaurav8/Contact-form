0ca099d073792c9adcd95c79881fb3872480b784
Gaurav Verma <gaurav.verma382001@gmail.com>
2023-04-22 08:28:15 +0530

    Updated Contact Form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ContactForm() {
+    const[status, setStatus] = useState('submit');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus('Sending...');
+        const { name, email, message } = e.target.elements;
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        };
+
+        let response = await fetch('http://localhost:5000/contact', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(details),
+        });
+        setStatus('Submit');
+        let result =  await  response.json();
+        alert(result.status);
+    }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <div>
             <label htmlFor='name'>Name:</label>
             <input type='text' id='name' required />
@@ -15,7 +39,7 @@ function ContactForm() {
             <label htmlFor='message'>Message:</label>
             <textarea id='message' required />
         </div>
-        <button type='submit'></button>
+        <button type='submit'>{status}</button>
     </form>
   )
 }
