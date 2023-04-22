20fca2b94928827e7808d7e574d4d570b8166a40
Gaurav Verma <gaurav.verma382001@gmail.com>
2023-04-22 08:28:38 +0530

    Final Commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 dotenv.config();
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
